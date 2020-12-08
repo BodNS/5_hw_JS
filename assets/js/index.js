6ccdb983a53154036a0d47c5c7a2b12b75229e32
BodNS <bondarenokn@gmail.com>
2020-12-08 22:43:50 +0200

    3 commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,11 @@ let a, h;
 a = parseFloat(prompt('Введите длину стороны треугольника a:'));
 h = parseFloat(prompt('Введите длину высоты треугольника h:'));
 console.log(`Площадь треугольника равна:`, 1/2*a*h);
-*//*
+*/ /*
 //task2
 let b = parseFloat(prompt('Введите длину в метрах:'));
 console.log(`${b} метра(ов) равно ${b*100} сантиметр(ов) или ${b*39.37} дюймов`);
-*//*
+*/ /*
 //task3
 let i,n;
 for (i=1; i<=9; i++){
@@ -16,7 +16,7 @@ for (i=1; i<=9; i++){
         console.log(`${i} * ${n} = ${i*n}`);
     }
 }
-*//*
+*/ /*
 //task4
 let price  = parseFloat(prompt('Введите стоимость пиццы за 100 гр., грн. :'));
 let ves = parseFloat(prompt('Введите вес пиццы, гр.:'))/100;
@@ -26,5 +26,27 @@ if (price*ves > 500 || ves > 10) {
 } else {
     console.log(`Стоимость заказа ${price*ves} грн.`);
 }
-*/
+*/ /*
 //task5
+let i, stepen = 1;
+for (i = 1; i <= 9; i++) {
+  stepen *= 2;
+  console.log(`2 в степени ${i} равно ${stepen}`);
+}
+*/ /*
+//task 6
+let x;
+for (x = -2; x <= 2; x += 0.5) {
+  console.log(
+    `При х равном ${x} y = -3x*x + 5x - 2 равно ${-3 * x * x + 5 * x - 2}`
+  );
+}
+*/
+//task7
+let q = parseFloat(prompt("Введите делимое:"));
+let w = parseFloat(prompt("Введите делитель:"));
+let answer;
+do {
+  answer = parseFloat(prompt(`Решите пример ${q} / ${w} =`));
+} while (answer != q / w);
+alert(`Вы правильно ответили!`);
