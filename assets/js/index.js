1b0a14d8146e1cfab543e4cca82900251d001ce4
BodNS <bondarenokn@gmail.com>
2020-12-09 16:30:47 +0200

    final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,7 +41,7 @@ for (x = -2; x <= 2; x += 0.5) {
     `При х равном ${x} y = -3x*x + 5x - 2 равно ${-3 * x * x + 5 * x - 2}`
   );
 }
-*/
+*//*
 //task7
 let q = parseFloat(prompt("Введите делимое:"));
 let w = parseFloat(prompt("Введите делитель:"));
@@ -50,3 +50,40 @@ do {
   answer = parseFloat(prompt(`Решите пример ${q} / ${w} =`));
 } while (answer != q / w);
 alert(`Вы правильно ответили!`);
+*//*
+//task8
+let i, p;
+do {
+p = parseInt(prompt("Введите целое число больше 0:"));
+} while (p <= 0);
+
+if (p==1 || p == 2) {
+    console.log(`Число простое`); 
+   
+} else {
+    for (i=2; i < p; i++) {
+        if (p % i == 0) {
+            console.log(`Число ${p} составное`); 
+            break;
+        } 
+    console.log(`Число ${p} простое`); 
+    break;
+    }
+}
+*//*
+//task9
+let u = -1, k;
+k = parseInt(Math.random()*10);
+while ( u != k) {
+   u = parseInt(prompt("Компъютер загадал число от 1 до 10, угадайте его: "));
+   if (u < k) {
+       alert('Загаданное число больше');
+   } 
+   else if (u > k)  {
+       alert('Загаданное число меньше');
+   }
+   else {
+       alert ('Вы угадали!');
+    }
+} 
+*/
